39eac52ebc87976db66629b4bc99442700a26c3a
phebz-austin <phebeaustin1@gmail.com>
2023-05-15 06:16:36 -0700

    updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,17 +7,11 @@
 /**
 		
  *  * count_word - helper function to count the number of words in a string
-		
  *   * @s: string to evaluate
-		
  *    *
-		
  *     * Return: number of words
-		
  *      */
-		
-int count_word(char *s)
-		
+int count_word(char *s)		
 {
 		
 		int flag, c, w;
